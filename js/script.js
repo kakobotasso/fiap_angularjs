d2c8613c2a764ee83ca9701626a005ec20789e92
Carlyle Botasso Junior <carlyle@vagas.com.br>
2017-04-25 08:48:36 -0300

    Criando tela de agendamento

--- js/script.js
+++ js/script.js
@@ -29,10 +29,41 @@ app.config(['$routeProvider', function ($routeProvider) {
     alert('Erro reportado: ' + error);
   });
 }])
-.controller('Agenda', [function (){
+.controller('Agenda', ['$http', function ($http) {
   var self = this;
 
+  self.tratamentos = tratamentos.all();
+  self.medicos = [];
+  $http.get('raw/medicos.txt').then(function (response) {
+    self.medicos = response.data;
+  }, function (error) {
+    alert('Erro reportado: ' + error);
+  });
+
   self.submit = function () {
-    var agendamento = {}; // POPULA O AGENDAMENTO  
+    var data = new Date(self.data);
+    var horario = new Date(self.hora);
+
+    var dia = (data.getDate() < 10) ? "0" + data.getDate() : data.getDate();
+    var mes = ((data.getMonth() + 1) < 10) ? ("0" + (data.getMonth() + 1)) : (data.getMonth() + 1);
+    var data_formatada = dia + "/" + mes + "/" + data.getFullYear();
+
+    var hora = (horario.getHours() < 10) ? "0" + horario.getHours() : horario.getHours();
+    var minuto = (horario.getMinutes() < 10) ? "0" + horario.getMinutes() : horario.getMinutes();
+    var hora_formatada = hora + ":" + minuto;
+
+    var agendamento = {
+      data_hora: data_formatada + " " + hora_formatada,
+      tipo_consulta: self.tipo_consulta,
+      tratamento: self.tratamento,
+      medico: self.medico
+    };
+
+    alert("Agendamento confirmado")
+
+    self.retorno =  agendamento["tratamento"] +
+                    " com o(a) Dr(a). " + agendamento["medico"] +
+                    " confirmado para " + agendamento["data_hora"] +
+                    " (" + agendamento["tipo_consulta"] + ")"
   };
 }])
